2dca385c22c1a3248851cfafb6c506d83f2c2034
BlandVen <117951794+BlandVen@users.noreply.github.com>
2023-10-20 19:58:44 +0300

    Update exer2.py

--- exer2.py
+++ exer2.py
@@ -1,3 +1,12 @@
 import os
 path = str(input("Input path: "))
-print(len(os.listdir(path)))
+print(get_num_files(path))
+def get_num_files(path):
+    num_files = 0
+    for item in os.listdir(path):
+        full_path = os.path.join(path, item)
+        if os.path.isfile(full_path):
+            num_files += 1
+        if os.path.isdir(full_path):
+            num_files += get_num_files(full_path)
+    return num_files
